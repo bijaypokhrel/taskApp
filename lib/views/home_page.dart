9f79cb277df831f1927b1742dec81c0b762f921c
Bijay Pokhrel <pokhrelbijay44@gmail.com>
2022-05-09 11:38:00 +0545

    created a card and search button and listview in home page

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -19,6 +19,7 @@ class _HomePageState extends State<HomePage> {
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 25.0),
               child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   //name
                   Column(
@@ -32,7 +33,7 @@ class _HomePageState extends State<HomePage> {
                         ),
                       ),
                       SizedBox(
-                        height: 8,
+                        height: 12,
                       ),
                       Text(
                         'Arjun Yudhisthir',
@@ -43,6 +44,147 @@ class _HomePageState extends State<HomePage> {
                       ),
                     ],
                   ),
+                  //profile picture
+                  Container(
+                    padding: const EdgeInsets.all(12),
+                    decoration: BoxDecoration(
+                      color: Colors.deepPurple[100],
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: const Icon(Icons.person),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            //card
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Container(
+                padding: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.pink[100],
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Row(
+                  children: [
+                    //animation or cute picture
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.deepPurple[200],
+                    ),
+                    const SizedBox(
+                      width: 20,
+                    ),
+                    // how do you feel + get started button
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            'How do you feel?',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 8,
+                          ),
+                          const Text(
+                            'Fill out your medical card right now',
+                            style: TextStyle(
+                              fontSize: 14,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 8,
+                          ),
+                          Container(
+                            padding: const EdgeInsets.all(12),
+                            decoration: BoxDecoration(
+                              color: Colors.deepPurple[300],
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            child: const Center(
+                              child: Text(
+                                'Get Started',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            //search bar
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 25.0,
+              ),
+              child: Container(
+                padding: const EdgeInsets.all(12),
+                decoration: BoxDecoration(
+                  color: Colors.deepPurple[100],
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: const TextField(
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(
+                      Icons.search,
+                    ),
+                    border: InputBorder.none,
+                    hintText: 'How can we help you?',
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            //horizontal list view
+            Container(
+              height: 80,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(20),
+                    color: Colors.deepPurple[100],
+                    child: Row(
+                      children: [
+                        Image.asset(
+                          'assets/icons/tooth.png',
+                          height: 50,
+                        ),
+                        const Text('Dentist'),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(20),
+                    color: Colors.deepPurple[100],
+                    child: Row(
+                      children: [
+                        Image.asset(
+                          'assets/icons/tooth.png',
+                          height: 50,
+                        ),
+                        const Text('Dentist'),
+                      ],
+                    ),
+                  )
                 ],
               ),
             )
